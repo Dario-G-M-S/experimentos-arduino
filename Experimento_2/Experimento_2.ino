8f8d821f7b75179592e60ff92eac14c4588bf558
Jose <jagarciateem@gmail.com@>
2022-03-28 17:45:02 +0200

    modificacion nombre carpetas y experimento 2 subido

---
+++ Experimento_2/Experimento_2.ino
@@ -0,0 +1,14 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop()
+{
+  int raw = analogRead(A0);
+  Serial.println("Temperatura: " + String(raw*500.0/1023.0) + "ยบC");
+  delay(1000); // Wait for 1000 millisecond(s)
+}
