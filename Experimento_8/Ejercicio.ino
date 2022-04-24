cb5f179515c1d78d53dd5a69b81a577e229af2a5
Jose <jagarciateem@gmail.com@>
2022-04-24 19:27:17 +0200

    modificado experimento8_branch

--- Experimento_8/Ejercicio.ino
+++ Experimento_8/Ejercicio.ino
@@ -1,1 +1,14 @@
-//Where is the code?
+//Jose Antonio García Ortiz
+//Darío González Martínez de Salazar
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(6, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop()
+{
+  digitalWrite(6,digitalRead(2));
+}
